89353f5ee4725e3f16a35165c0aad6cc77d26924
Jérémie QUATORZE <jquatorze@nsi.admr.org>
2024-02-04 00:45:56 +0100

    first

---
+++ src/bot/commands/play.command.ts
@@ -0,0 +1,27 @@
+import { SlashCommandPipe } from '@discord-nestjs/common';
+import {
+  Command,
+  EventParams,
+  Handler,
+  InteractionEvent,
+} from '@discord-nestjs/core';
+import { ClientEvents } from 'discord.js';
+
+import { PlayDto } from '../dto/play.dto';
+
+@Command({
+  name: 'play',
+  description: 'Plays a song',
+})
+export class PlayCommand {
+  @Handler()
+  onPlayCommand(
+    @InteractionEvent(SlashCommandPipe) dto: PlayDto,
+    @EventParams() args: ClientEvents['interactionCreate'],
+  ): string {
+    console.log('DTO', dto);
+    console.log('Event args', args);
+
+    return `Start playing ${dto.song}.`;
+  }
+}
